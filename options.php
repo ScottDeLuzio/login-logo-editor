89f7e29ff2bae690f5a62aada8353e85d1b2b25a
scott.deluzio <scott.deluzio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-06 22:21:48 +0000

    Tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/login-logo-editor-by-oizuled/tags/1.2@852841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,26 +1,31 @@
 <div class="wrap">
 	<div class="postbox">
-		<h2>Login Logo Options</h2>
+		<h2><?php _e('Login Logo Options', 'login_translate'); ?></h2>
 		<form method='post' action='options.php'>
 			<?php wp_nonce_field( 'update-options' ); ?>
 			<?php settings_fields( 'oizuled-login-logo-option-group' ); ?>
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row"><strong>Enter the URL of the image you want to use for your login logo</strong>.</th>
-					<td><input type="text" size="36" name="oizuled-login-logo-img" value="<?php echo get_option('oizuled-login-logo-img'); ?>" />
+					<th scope="row"><strong><?php _e('Enter a URL or upload an image to use for your login logo', 'login_translate'); ?></strong>.</th>
+					<td>
+						<label for="upload_image">
+							<input id="upload_image" type="text" size="36" name="oizuled-login-logo-img" value="<?php echo get_option('oizuled-login-logo-img'); ?>" /> 
+							<input id="upload_image_button" class="button" type="button" value="<?php _e('Upload Image', 'login_translate'); ?>" />
+						</label>
+					</td>
 				</tr>
 				<tr valign="top">
-					<th scope="row"><strong>Enter the URL of you would like the login logo to point to.</strong><br />By default it points to <a href="http://wordpress.org" target="_blank">WordPress.org</a></th>
+					<th scope="row"><strong><?php _e('Enter the URL of you would like the login logo to point to.', 'login_translate'); ?></strong><br /><?php _e('By default it points to', 'login_translate'); ?> <a href="http://wordpress.org" target="_blank">WordPress.org</a></th>
 					<td><input type="text" size="36" name="oizuled-login-logo-link" value="<?php echo get_option('oizuled-login-logo-link'); ?>" />
 				</tr>
 				<tr valign="top">
-					<th scope="row"><strong>Enter the text to display when you hover your mouse over the login logo.</strong><br />By default it says "Powered by WordPress".</th>
+					<th scope="row"><strong><?php _e('Enter the text to display when you hover your mouse over the login logo.', 'login_translate'); ?></strong><br /><?php _e('By default it says "Powered by WordPress".', 'login_translate'); ?></th>
 					<td><input type="text" size="36" name="oizuled-login-logo-title" value="<?php echo get_option('oizuled-login-logo-title'); ?>" />
 				</tr>
 				<tr valign="top">
-					<th scope="row"><strong>Enter custom CSS for the login screen.</strong><br />If you are not familiar with CSS, you can leave this blank.
+					<th scope="row"><strong><?php _e('Enter custom CSS for the login screen.', 'login_translate'); ?></strong><br /><?php _e('If you are not familiar with CSS, you can leave this blank.', 'login_translate'); ?>
 					<ul>
-						<li><a href="http://codex.wordpress.org/Customizing_the_Login_Form#Styling_Your_Login" target="_blank">CSS options can be found by clicking here.</a></li>
+						<li><a href="http://codex.wordpress.org/Customizing_the_Login_Form#Styling_Your_Login" target="_blank"><?php _e('CSS options can be found by clicking here.', 'login_translate'); ?></a></li>
 					</ul>
 					</th>
 					<td><textarea rows="10" cols="36" name="oizuled-login-logo-css"><?php echo get_option('oizuled-login-logo-css'); ?></textarea>
@@ -36,18 +41,19 @@
 		$oizuledtitle = get_option('oizuled-login-logo-title');
 		$oizuledcss = get_option('oizuled-login-logo-css');
 		if(!empty($oizuledlogo)) { ?>
-			<p>How your login logo image looks:</p>
+			<p><?php _e('How your login logo image looks:', 'login_translate'); ?></p>
 			<a href="<?php if (!empty($oizuledlink)) { echo $oizuledlink; } else { echo "#"; } ?>">
 				<img src="<?php echo $oizuledlogo; ?>" title="<?php if (!empty($oizuledtitle)) { echo $oizuledtitle; } else { echo ""; } ?>" />
 			</a>	
 		<?php } ?>
 	</div>
 	<div class="postbox">
-		<p>If this plugin has helped you out at all, please consider making a donation to encourage future updates.<br />Your generosity is appreciated!</p>
+		<p><?php _e('If this plugin has helped you out at all, please consider making a donation to encourage future updates.', 'login_translate'); ?><br /><?php _e('Your generosity is appreciated!', 'login_translate'); ?></p>
 			<a href="#" onclick="window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5WVZQ3MZAKTU2');">
 				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" width="147" height="47">
 			</a>
-		<p>To report any issues with this plugin, please use the <a href="http://oizuled.com/contact">contact form</a>, or visit the support page on WordPress.org.</p>
+		<p><?php _e('To report any issues with ', 'login_translate'); ?><strong><?php _e('this plugin', 'login_translate'); ?></strong><?php _e(', please visit the', 'login_translate'); ?> <a href="http://wordpress.org/support/plugin/login-logo-editor-by-oizuled"><?php _e('support page on WordPress.org', 'login_translate'); ?></a>.</p>
+		<p><?php _e('For all other WordPress support, please check out the following', 'login_translate'); ?> <a href="http://oizuled.com/wordpress-site-setup/"><?php _e('site set-up', 'login_translate'); ?></a>, <a href="http://oizuled.com/wordpress-support-24x7-unlimited-fast-fixes/"><?php _e('24x7 support', 'login_translate'); ?></a><?php _e(', and other', 'login_translate'); ?> <a href="http://oizuled.com/live-wordpress-support-services/"><?php _e('WordPress training', 'login_translate'); ?></a> <?php _e('services', 'login_translate'); ?>.</p>
 		<p><a href="https://twitter.com/oizuled" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @oizuled</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
 	</div>
